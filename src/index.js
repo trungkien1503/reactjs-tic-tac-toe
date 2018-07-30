eabd02c69e246e0d9c2f761b2bf340d4f18c0027
Apple <apple@Apples-MacBook-Pro.local>
2018-07-30 14:55:15 +0700

    Init constructor for Board

--- src/index.js
+++ src/index.js
@@ -20,6 +20,12 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
     return <Square value={i} />;
   }
@@ -72,3 +78,4 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
